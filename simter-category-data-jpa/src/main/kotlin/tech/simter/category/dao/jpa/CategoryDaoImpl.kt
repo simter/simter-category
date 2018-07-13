ef8faaad4eaeee34dd7999bded251df11bb0c5dd
Smart-Jie <smartjie23@gmail.com>
2018-07-13 08:54:56 +0800

    Add findChild Dao interface and JPA implementation

--- simter-category-data-jpa/src/main/kotlin/tech/simter/category/dao/jpa/CategoryDaoImpl.kt
+++ simter-category-data-jpa/src/main/kotlin/tech/simter/category/dao/jpa/CategoryDaoImpl.kt
@@ -2,7 +2,9 @@ package tech.simter.category.dao.jpa
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import tech.simter.category.dao.CategoryDao
+import tech.simter.category.po.Category
 import javax.persistence.EntityManager
 import javax.persistence.PersistenceContext
 
@@ -10,9 +12,26 @@ import javax.persistence.PersistenceContext
  * The JPA implementation of [CategoryDao].
  *
  * @author RJ
+ * @author JF
  */
 @Component
 class CategoryDaoImpl @Autowired constructor(
   @PersistenceContext private val em: EntityManager,
   private val repository: CategoryJpaRepository
-) : CategoryDao
+) : CategoryDao {
+  override fun findChild(parentParentId: Int, parentSNs: Array<String>, childStatuses: Array<Category.Status>): Flux<Category> {
+    val hql = """
+      select c from Category c
+      inner join Category p on p.pid.id = :pPid and p.sn in (:pSNs)
+      where c.pid = p.id and c.status in (:cStatuses)
+      order by p.sn asc, c.status asc, c.sn asc
+      """.trimIndent()
+    return Flux.fromIterable(
+      em.createQuery(hql, Category::class.java)
+        .setParameter("pPid", parentParentId)
+        .setParameter("pSNs", parentSNs.toList())
+        .setParameter("cStatuses", childStatuses.toList())
+        .resultList
+    )
+  }
+}
--- simter-category-data-reactive-mongo/src/main/kotlin/tech/simter/category/dao/reactive/mongo/CategoryDaoImpl.kt
+++ simter-category-data-reactive-mongo/src/main/kotlin/tech/simter/category/dao/reactive/mongo/CategoryDaoImpl.kt
@@ -2,7 +2,9 @@ package tech.simter.category.dao.reactive.mongo
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
+import reactor.core.publisher.Flux
 import tech.simter.category.dao.CategoryDao
+import tech.simter.category.po.Category
 
 /**
  * The Reactive MongoDB implementation of [CategoryDao].
@@ -12,4 +14,8 @@ import tech.simter.category.dao.CategoryDao
 @Component
 class CategoryDaoImpl @Autowired constructor(
   private val repository: CategoryReactiveRepository
-) : CategoryDao
+) : CategoryDao {
+  override fun findChild(parentParentId: Int, parentSNs: Array<String>, childStatuses: Array<Category.Status>): Flux<Category> {
+    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+  }
+}
