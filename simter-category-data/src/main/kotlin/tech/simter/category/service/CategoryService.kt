59aed2eb8f541ce1208913249db216c095aafaf9
Smart-Jie <smartjie23@gmail.com>
2018-07-13 09:09:57 +0800

    Add findChild Service interface and implementation

--- simter-category-data/src/main/kotlin/tech/simter/category/service/CategoryService.kt
+++ simter-category-data/src/main/kotlin/tech/simter/category/service/CategoryService.kt
@@ -1,10 +1,26 @@
 package tech.simter.category.service
 
+import reactor.core.publisher.Flux
+import tech.simter.category.po.Category
+
 /**
  * The Service Interface.
  *
  * This interface is design for all external modules to use.
  *
  * @author RJ
+ * @author JF
  */
-interface CategoryService
+interface CategoryService {
+  /**
+   * Find child categories by the parent's sn under the parent's parent's id.
+   *
+   * The return result order by parent's sn asc + child's status asc + child's sn asc.
+   *
+   * @param [parentParentId] parent id of the parent sn
+   * @param [parentSNs] parent sn of the child
+   * @param [childStatuses] child status
+   * @return [Flux] emitting the child category if exists or [Flux.empty] otherwise
+   */
+  fun findChild(parentParentId: Int, parentSNs: Array<String>, childStatuses: Array<Category.Status>): Flux<Category>
+}
--- simter-category-data/src/main/kotlin/tech/simter/category/service/CategoryServiceImpl.kt
+++ simter-category-data/src/main/kotlin/tech/simter/category/service/CategoryServiceImpl.kt
@@ -3,10 +3,21 @@ package tech.simter.category.service
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
+import reactor.core.publisher.Flux
 import tech.simter.category.dao.CategoryDao
+import tech.simter.category.po.Category
 
+/**
+ * The Service implementation of [CategoryService].
+ *
+ * @author JF
+ */
 @Component
 @Transactional
 class CategoryServiceImpl @Autowired constructor(
   private val dao: CategoryDao
-) : CategoryService
+) : CategoryService {
+  override fun findChild(parentParentId: Int, parentSNs: Array<String>, childStatuses: Array<Category.Status>): Flux<Category> {
+    return dao.findChild(parentParentId, parentSNs, childStatuses)
+  }
+}
